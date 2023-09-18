e890a0b2e07ae0dc32d02c8a4937b0903fd392ac
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-09-18 21:06:13 +0545

    Complete : result css desktop

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -44,14 +44,48 @@ const Result = () => {
 
 export default Result;
 
-const Wrapper = styled.table``;
+const Wrapper = styled.table`
+  padding: 30px;
+  border-radius: 5px;
+  background: #24262a;
+`;
 
-const TableHead = styled.thead``;
+const TableHead = styled.thead`
+  margin-bottom: 10px;
+  firs
+`;
 
-const Row = styled.tr``;
+const Row = styled.tr`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  margin-bottom: 15px;
+`;
 
-const Head = styled.th``;
+const Head = styled.th`
+  color: #eceff2;
+  font-family: Montserrat;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  width: 150px;
+  text-align: right;
+  &:first-of-type {
+    width: 40px;
+  }
+`;
 
 const TabelBody = styled.tbody``;
 
-const Content = styled.td``;
+const Content = styled.td`
+  color: #eceff2;
+  font-family: Montserrat;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  width: 150px;
+  text-align: right;
+  &:first-of-type {
+    width: 40px;
+  }
+`;
